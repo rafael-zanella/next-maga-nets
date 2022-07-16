b11e362ceb04b37b94accfdebede3f0f279f9fab
Rafael Zanella <rafaels.zanella@gmail.com>
2022-07-16 00:52:59 -0300

    style: adicionar grid layout

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document'
+import { useEffect } from 'react'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
@@ -29,13 +30,22 @@ export default class MyDocument extends Document {
   }
 
   render () {
+    useEffect(() => {
+      document.createElement('header')
+      document.createElement('nav')
+      document.createElement('section')
+      document.createElement('article')
+      document.createElement('aside')
+      document.createElement('footer')
+    })
+
     return (
       <Html lang='pt'>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" cross-origin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
-
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <link rel='shortcut icon' href='/favicon.ico' />
         </Head>
 
