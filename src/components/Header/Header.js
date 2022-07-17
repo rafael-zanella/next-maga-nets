88c41922c57da6119b8869dc2ddda2c624d26760
Rafael Zanella <rafaels.zanella@gmail.com>
2022-07-16 21:59:48 -0300

    feat: adicionar header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import {
+  Container,
+  Infos,
+  Logo,
+  City,
+  Phone,
+  Wishlist as WishlistAccess,
+  InputArea,
+} from './Header.styled'
+
+import { GridContainer } from '@/components/GridContainer'
+
+export function Header () {
+  return (
+    <Container>
+      <GridContainer className='clearfix'>
+        <div className='row'>
+          <div className='col col-2 col-sm-3'>
+            <Logo><a href='/'>Maga Nets</a></Logo>
+          </div>
+
+          <div className='col col-10 col-sm-9'>
+            <Infos className='row'>
+              <City className='col col-4'>Cidade: Gravataí</City>
+              <Phone className='col col-4'>Central de atendimento</Phone>
+              <WishlistAccess className='col col-4'>
+                <a href='/wishlist'>Lista de desejos</a> 
+              </WishlistAccess>
+            </Infos>
+
+            <InputArea className='row'>
+              <div className='col col-12'>
+                <input style={{ width: '100%' }} />
+              </div>
+            </InputArea>
+          </div>
+        </div>
+      </GridContainer>
+    </Container>
+  )
+}
