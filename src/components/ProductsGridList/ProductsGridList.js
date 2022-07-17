bc711e03b71d3df92bb78da8d2cb25a461a9453c
Rafael Zanella <rafaels.zanella@gmail.com>
2022-07-17 07:45:47 -0300

    fix: remover mock de produtos e alterar pagina inicial para exibir produtos da api

--- src/components/ProductsGridList/ProductsGridList.js
+++ src/components/ProductsGridList/ProductsGridList.js
@@ -2,72 +2,62 @@
 import { Card } from '../Card/Card'
 import { GridContainer } from '../GridContainer'
 import PropTypes from 'prop-types'
+import { currencyFormatter } from '@/utils/currencyFormatter'
 
 
-export const ProductsGridList = ({ onClickProduct }) => {
+
+
+export const ProductsGridList = ({ products, onClickProduct }) => {
   return (
     <GridContainer className='clearfix'>
       <div className="row">
-        <div className="col col-12 col-sm-6 col-md-4 col-lg-3">
-          <Card
-            id="1"
-            title="Produto para bebe 2"
-            imageAlt="alt"
-            imageURL="https://firebasestorage.googleapis.com/v0/b/my-news-app-ad6d7.appspot.com/o/maganets%2Fshirt.svg?alt=media&token=e1534d49-4270-4929-a0ec-0649acfbfff2"
-            isDesired={false}
-            price="R$ 4,99"
-            onClick={onClickProduct}
-          />
-        </div>
-        <div className="col col-12 col-sm-6 col-md-4 col-lg-3">
-          <Card
-            id="2"
-            title="Produto para bebe"
-            imageAlt="alt"
-            imageURL="https://firebasestorage.googleapis.com/v0/b/my-news-app-ad6d7.appspot.com/o/maganets%2Fshirt.svg?alt=media&token=e1534d49-4270-4929-a0ec-0649acfbfff2"
-            isDesired={true}
-            price="R$ 4,99"
-            onClick={onClickProduct}
-          />
-        </div>
-        <div className="col col-12 col-sm-6 col-md-4 col-lg-3">
-          <Card
-            id="3"
-            title="Produto para bebe"
-            imageAlt="alt"
-            imageURL="https://firebasestorage.googleapis.com/v0/b/my-news-app-ad6d7.appspot.com/o/maganets%2Fshirt.svg?alt=media&token=e1534d49-4270-4929-a0ec-0649acfbfff2"
-            isDesired={false}
-            price="R$ 4,99"
-            onClick={onClickProduct}
-          />
-        </div>
-        <div className="col col-12 col-sm-6 col-md-4 col-lg-3">
-          <Card
-            id="4"
-            title="Produto para bebe"
-            imageAlt="alt"
-            imageURL="https://firebasestorage.googleapis.com/v0/b/my-news-app-ad6d7.appspot.com/o/maganets%2Fshirt.svg?alt=media&token=e1534d49-4270-4929-a0ec-0649acfbfff2"
-            isDesired={false}
-            price="R$ 4,99"
-            onClick={onClickProduct}
-          />
-        </div>
-        <div className="col col-12 col-sm-6 col-md-4 col-lg-3">
-          <Card
-            id="5"
-            title="Produto para bebe"
-            imageAlt="alt"
-            imageURL="https://firebasestorage.googleapis.com/v0/b/my-news-app-ad6d7.appspot.com/o/maganets%2Fshirt.svg?alt=media&token=e1534d49-4270-4929-a0ec-0649acfbfff2"
-            isDesired={false}
-            price="R$ 4,99"
-            onClick={onClickProduct}
-          />
-        </div>
+
+        {
+          !products && (
+            <>Ocorreu um erro ao buscar os produtos!</>
+          )
+        }
+
+        {
+          products?.length === 0 && (
+            <>Nenhum produto encontrado!</>
+          )
+        }
+
+        {
+          products && products.map(product => (
+            <div className="col col-12 col-sm-6 col-md-4 col-lg-3" key={product.id}>
+              <Card
+                id={product.id.toString()}
+                title={product.title}
+                imageAlt={`${product.title}, ${product}`}
+                imageURL="https://firebasestorage.googleapis.com/v0/b/my-news-app-ad6d7.appspot.com/o/maganets%2Fshirt.svg?alt=media&token=e1534d49-4270-4929-a0ec-0649acfbfff2"
+                isDesired={false} // todo
+                price={currencyFormatter('pt-BR', product.currencyId).format(product.price)}
+                onClick={onClickProduct}
+              />
+            </div>
+          ))
+        }
       </div>
     </GridContainer>
   )
 }
 
 ProductsGridList.propTypes = {
-  onClickProduct: PropTypes.func
+  products: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number,
+    sku: PropTypes.number,
+    title: PropTypes.string,
+    description: PropTypes.string,
+    availableSizes: PropTypes.object,
+    style: PropTypes.string,
+    price: PropTypes.number,
+    installments: PropTypes.number,
+    currencyId: PropTypes.string,
+    currencyFormat: PropTypes.string,
+    isFreeShipping: PropTypes.bool,
+    image: PropTypes.string
+  })),
+  onClickProduct: PropTypes.func,
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,35 @@
 import { Breadcrumb } from '@/components/Breadcrumb/Breadcrumb'
 import { Header } from '@/components/Header/Header'
 import { ProductsGridList } from '@/components/ProductsGridList/ProductsGridList'
+import { getAllProducts } from '@/services/products/products.service'
 
-export default function Home() {
+import PropTypes from 'prop-types'
+
+
+export async function getServerSideProps () {
+  let data  
+  data = await getAllProducts()
+
+  return {
+    props: { data }
+  }
+}
+
+export default function Home(props) {
   return (
     <div>
       <Header />
       <Breadcrumb crumbs={[]} />
-      <ProductsGridList onClickProduct={() => console.log('click')} />
+
+      <ProductsGridList
+        products={props.data?.products}
+        onClickProduct={() => console.log('click')} 
+      />
     </div>
   )
+}
+
+Home.propTypes = {
+  props: PropTypes.object,
+  data: PropTypes.object
 }
