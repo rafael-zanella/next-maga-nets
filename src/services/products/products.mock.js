ac25366e858441463f693e0f324d1c0441e86525
Rafael Zanella <rafaels.zanella@gmail.com>
2022-07-17 07:43:13 -0300

    feat: adicionar service para consultar lista de produtos

---
+++ src/services/products/products.mock.js
@@ -0,0 +1,59 @@
+export const productsMock = {
+  products: [
+    {
+      id: 200,
+      sku: 8552515751438644,
+      title: "Camisa Nike Corinthians I",
+      description: "14/15 s/nº",
+      availableSizes: {
+        S: 100,
+        G: 5,
+        GG: 120,
+        GGG: 12
+      },
+      style: "Branco com listras pretas",
+      price: 229.9,
+      installments: 9,
+      currencyId: "BRL",
+      currencyFormat: "R$",
+      isFreeShipping: true,
+      image: "https://via.placeholder.com/300x300"
+    },
+    {
+      id: 1,
+      sku: 18644119330491310,
+      title: "Camisa Nike Corinthians II",
+      description: "14/15 s/nº",
+      availableSizes: {
+        S: 100,
+        G: 5,
+        GG: 120,
+        GGG: 12
+      },
+      style: "Preta com listras brancas",
+      price: 229.9,
+      installments: 9,
+      currencyId: "BRL",
+      currencyFormat: "R$",
+      isFreeShipping: true,
+      image: "https://via.placeholder.com/300x300"
+    },
+    {
+      id: 2,
+      sku: 11854078013954528,
+      title: "Camisa Feminina Nike Corinthians I",
+      description: "14/15 s/nº",
+      availableSizes: {
+        S: 1,
+        G: 6
+      },
+      style: "Branco com listras pretas",
+      price: 199.9,
+      installments: 7,
+      currencyId: "BRL",
+      currencyFormat: "R$",
+      isFreeShipping: true,
+      image: "https://via.placeholder.com/300x300"
+    }
+  ]
+}
